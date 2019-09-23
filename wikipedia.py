0c75a72575e41ff376eb041dd80f455c3c121990
johnsliao <john@johnliao.org>
2019-09-23 16:50:55 -0400

    WIP

--- wikipedia.py
+++ wikipedia.py
@@ -1,4 +1,7 @@
 import wikipediaapi
+import time
+
+from selenium import webdriver
 
 
 def fetch(q):
@@ -12,8 +15,31 @@ def fetch(q):
 
 
 def scrape():
-    url = 'https://en.wikipedia.org/wiki/Category:Living_people'
+    with open('living_people.csv', 'w'):
+        pass
+
+    try:
+        driver = webdriver.Chrome(r'./chromedriver')
+        driver.get('https://en.wikipedia.org/wiki/Category:Living_people')
+
+        for page in range(5000):
+            print('Parsing page {}'.format(page))
+            groups = driver.find_elements_by_xpath("//div[@class='mw-category-group']")
+
+            with open('living_people.csv', 'a') as fs:
+                for group in groups:
+                    fs.write(group.text)
+                    fs.write('\n')
+
+            next_page = driver.find_element_by_xpath(
+                '//*[@id="mw-pages"]/a[2]') if page == 0 else driver.find_element_by_xpath('//*[@id="mw-pages"]/a[4]')
+            next_page.click()
+            time.sleep(1)
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
 
 
 if __name__ == '__main__':
-    pass
+    scrape()
