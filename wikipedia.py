7d701d806eb0a507030097c9971aefd6826b5c0c
johnsliao <john@johnliao.org>
2019-09-23 16:26:52 -0400

    WIP

--- wikipedia.py
+++ wikipedia.py
@@ -1,7 +1,19 @@
 import wikipediaapi
 
-wiki_wiki = wikipediaapi.Wikipedia('en')
 
-page_py = wiki_wiki.page('Python_(programming_language)')
-print(page_py.summary)
-print(page_py.categories)
+def fetch(q):
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+
+    page_py = wiki_wiki.page('Python_(programming_language)')
+    print(page_py.summary)
+
+    for category in page_py.categories:
+        print(category)
+
+
+def scrape():
+    url = 'https://en.wikipedia.org/wiki/Category:Living_people'
+
+
+if __name__ == '__main__':
+    pass
