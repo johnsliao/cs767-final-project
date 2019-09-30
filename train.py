ad7f6205f3f1c8e36263d68b7d049564b3786909
johnsliao <john@johnliao.org>
2019-09-30 15:54:53 -0400

    WIP

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from tensorflow.keras import layers
 from tensorflow import feature_column
-
+from sklearn.linear_model import LogisticRegression
 
 def remove_punct(text):
     return "".join([char for char in text if char not in string.punctuation])
@@ -35,83 +35,24 @@ def clean_text(text):
     return text
 
 
-sample = {'name': 'Michael Hardt',
-          'summary': 'Michael Hardt (born 1960) is an American political philosopher and literary theorist. Hardt is best known for his book Empire, which was co-written with Antonio Negri. It has been praised by Slavoj Žižek as the "Communist Manifesto of the 21st Century".Hardt and Negri suggest that several forces which they see as dominating contemporary life, such as class oppression, globalization and the commodification of services (or production of affects), have the potential to spark social change of unprecedented dimensions. A sequel, Multitude: War and Democracy in the Age of Empire was published in August 2004. It outlines an idea first propounded in Empire, which is that of the multitude as possible locus of a democratic movement of global proportions. The third and final part of the trilogy, Commonwealth, was published in 2009.',
-          'categories': ['1960 births', '20th-century American philosophers', '20th-century American writers',
-                         '21st-century American non-fiction writers', '21st-century American philosophers',
-                         'All BLP articles lacking sources', 'All articles with unsourced statements',
-                         'American Marxists', 'American communists', 'American literary critics',
-                         'American political philosophers', 'Articles with hCards',
-                         'Articles with unsourced statements from October 2015', 'Autonomism',
-                         'BLP articles lacking sources from October 2013', 'Continental philosophers',
-                         'Duke University faculty', 'European Graduate School faculty', 'Libertarian socialists',
-                         'Living people', 'Marxist theorists', 'People from Bethesda, Maryland',
-                         'People from Potomac, Maryland', 'Swarthmore College alumni',
-                         'University of Washington alumni', 'Wikipedia articles with BNF identifiers',
-                         'Wikipedia articles with GND identifiers', 'Wikipedia articles with ISNI identifiers',
-                         'Wikipedia articles with LCCN identifiers', 'Wikipedia articles with NDL identifiers',
-                         'Wikipedia articles with NKC identifiers', 'Wikipedia articles with NTA identifiers',
-                         'Wikipedia articles with SELIBR identifiers', 'Wikipedia articles with SUDOC identifiers',
-                         'Wikipedia articles with VIAF identifiers',
-                         'Wikipedia articles with WorldCat-VIAF identifiers']}
-
 with open('files/test_set.json', 'r') as fs:
     data = json.load(fs)
 
 df = pd.DataFrame(data['data'][0:100])
 df['cleaned_summary'] = df.summary.apply(clean_text)
 print(df.head())
-#
-# train, test = train_test_split(df, test_size=0.2)
-# train, val = train_test_split(train, test_size=0.2)
-# print(len(train), 'train examples')
-# print(len(val), 'validation examples')
-# print(len(test), 'test examples')
 
-# Create labels for categories
-# Create vocabulary for input text
-sentences = ['John likes ice cream', 'John hates chocolate.']
+# Vectorize sentences
 vectorizer = CountVectorizer(min_df=0, lowercase=False)
-vectorizer.fit(sentences)
-print(vectorizer.vocabulary_)
-print(vectorizer.transform(sentences).toarray())
-
 sentences = df['cleaned_summary'].values
+vectorizer.fit(sentences)
+
+# Vectorize labels
 labels = df['categories'].to_list()
-# print(labels)
 mlb = MultiLabelBinarizer()
 labels = mlb.fit_transform(labels)
-# print(labels)
 
 sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, labels, test_size=0.25,
                                                                     random_state=1000)
 
-model = tf.keras.Sequential([
-    layers.Dense(128, activation='relu'),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-])
 
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-print(sentences_train)
-ds = tf.data.Dataset.from_tensor_slices((sentences_train, y_train))
-ds = ds.batch(32)
-
-val_ds = tf.data.Dataset.from_tensor_slices((sentences_test, y_test))
-val_ds = val_ds.batch(32)
-
-H = model.fit(ds, validation_data=val_ds, epochs=20)
-
-# print(sentences_train)
-
-# summary = df.pop('summary')
-# dataset = tf.data.Dataset.from_tensor_slices((df.values, summary.values))
-
-# Pre-processing data
-# https://towardsdatascience.com/natural-language-processing-nlp-for-machine-learning-d44498845d5b
-
-#
-# tagged = nltk.pos_tag(tokens)
-# print(tagged)
